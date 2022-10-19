143974f47c1334fb86cfef3c3221a30c83bbfd1f
Usama Malik <usmamalik344@gmail.com>
2022-10-19 04:51:38 -0700

    Addrouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './Component/cart/cart.component';
+import { ProductsComponent } from './Component/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , redirectTo: 'products', pathMatch: 'full' },
+  {path: 'products' , component: ProductsComponent},
+  {path: 'cart' , component: CartComponent}
+];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
