a2e77774b0dcebe4890e5caf4427950b8ec52e35
Usama Malik <usmamalik344@gmail.com>
2022-10-19 02:56:04 -0700

    checkng Api

--- src/app/Component/products/products.component.ts
+++ src/app/Component/products/products.component.ts
@@ -14,6 +14,7 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
     this.api.getProduct().subscribe((res) => {
       this.ProductList = res;
+      console.log(res);
     });
   }
 }
