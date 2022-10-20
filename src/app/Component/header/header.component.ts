a51d6da008143a3b6ee3a01747488c5200ec0f13
Usama Malik <usmamalik344@gmail.com>
2022-10-20 02:56:01 -0700

    newChanges

--- src/app/Component/header/header.component.ts
+++ src/app/Component/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public cartCount: number = 0
+
+  constructor(private cartService : CartService) { }
 
   ngOnInit(): void {
+    this.cartService.getProduct().subscribe(res=>{
+      this.cartCount = res.length
+    }) 
+   
+
   }
 
 }
